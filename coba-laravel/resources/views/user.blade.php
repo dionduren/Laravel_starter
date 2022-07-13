c57254c6911317263cb6e542790724b950b9b914
dionduren <dionduren94@gmail.com>
2022-07-13 17:42:35 +0800

    sebelum pulang

--- coba-laravel/resources/views/user.blade.php
+++ coba-laravel/resources/views/user.blade.php
@@ -6,11 +6,15 @@
     <h1>Halaman Daftar Data</h1>
 
     @foreach ($data as $file)
-        <article class="mb-10">
+        <article class="mb-5 border-bottom pb-4">
             {{-- mengganti link menjadi slug  --}}
-            <h2><a href="/user/{{ $file->slug }}"> {{ $file->title }}</a></h2>
-            <h5> {{ $file->excerpt }}</h5>
+            <h2><a href="/user/{{ $file->slug }}" class="text-decoration-none"> {{ $file->title }}</a></h2>
+
+            <h6>By Dion Alamsah in <a href="/categories/{{ $file->category->slug }}" class="text-decoration-none">{{ $file->category->name }}</a> </h6>
+
+            <p> {{ $file->excerpt }}</p>
+
+            <a href="/user/{{ $file->slug }}" class="text-decoration-none"> Read more..</a>
         </article>
-        <br>
     @endforeach
 @endsection
